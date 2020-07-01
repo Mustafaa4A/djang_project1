4385630e306e4819de63c0ac60988918f9efa062
Mustafa <musdhafa42@gmail.com>
2020-07-01 07:50:34 +0300

    initial commit

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
 
 
-def homePageView(request):
-    return HttpResponse('<h1>Hello, World!</h1>')
+class HomePageView(TemplateView):
+    template_name = 'home.html'
