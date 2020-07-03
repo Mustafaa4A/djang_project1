2631a75e191092794a06743eb0ec6e84f41a841f
Mustafa Abubakar <musdhafa42@gmail.com>
2020-07-03 19:14:52 +0300

    New update from Heroku deploynment

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,13 @@
-from django.views.generic import TemplateView
-
+from django.views.generic import ListView, TemplateView
+from .models import Post
 # Create your views here.
 
 
-class HomePageView(TemplateView):
+class HomePageView(ListView):
+    model = Post
     template_name = 'home.html'
+    context_object_name = 'all_posts_list'
 
 
 class AboutPageView(TemplateView):
-    template_name = 'about.html'
+    template_name = 'home.html'
